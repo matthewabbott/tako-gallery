5927170c1c0788454cab68c776fe5b2994b5bdf7
matthewabbott <ttobbatam@gmail.com>
2025-05-08 12:30:42 +0100

    Loosen content security for font url

--- next.config.js
+++ next.config.js
@@ -2,14 +2,14 @@
 const nextConfig = {
     reactStrictMode: true,
     images: {
-        domains: ['trytako.com'],
+        domains: ['trytako.com', 'tako-static-assets-production.s3.amazonaws.com'],
         formats: ['image/avif', 'image/webp'],
         deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
         imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
         minimumCacheTTL: 60 * 60 * 24, // 24 hours
         dangerouslyAllowSVG: true,
         contentDispositionType: 'attachment',
-        contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+        contentSecurityPolicy: "default-src 'self'; font-src 'self' https://tako-static-assets-production.s3.amazonaws.com; script-src 'none'; sandbox;",
     },
     experimental: {
         optimizeCss: true,
